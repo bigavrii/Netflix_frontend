7265f0f57aa97793265cf2ddb23732cdcfd7ca91
bigavrii <ugoroka@gmail.com>
2026-04-16 22:11:53 +0100

    Dockerfile crtd

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://127.0.0.1:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    baseURL:'http://127.0.0.1:8080'
 });
